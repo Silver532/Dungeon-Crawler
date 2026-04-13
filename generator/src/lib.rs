6bb67c33ee18e842c515a5a28bb396ba4730ae8d
Silver532 <silver532mc@gmail.com>
2026-04-13 17:21:00 -0400

    Modified type annotation

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -1,11 +1,11 @@
-use ndarray::{ArrayBase, Dim, OwnedRepr};
+use ndarray::{Array2};
 
 mod stage_1;
 //mod stage_2;
 //mod stage_3;
 mod helpers;
 
-pub fn map_generator() -> ArrayBase<OwnedRepr<u8>, Dim<[usize; 2]>> {
+pub fn map_generator() -> Array2<u8> {
     let mut rng = helpers::init_rng(None);
     
     let layout = stage_1::generate_layout(&mut rng);
