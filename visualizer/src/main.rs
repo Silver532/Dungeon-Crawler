0713508e9c5d9a3bfd29d820bf786b7903276b5d
Silver532 <silver532mc@gmail.com>
2026-04-13 17:17:43 -0400

    Add basic room count testing Commented out so that there is only one main() at a time

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -1,11 +1,13 @@
+use ndarray::{Array2};
+
 use generator::map_generator;
 
 fn main() {
-    let layout = map_generator();
+    let layout: Array2<u8> = map_generator();
     let mut count: u32 = 0;
     for row in layout.rows() {
         for &val in row {
-            let c = match val {
+            let c: char = match val {
                 0 => '.',
                 _ => '#',
             };
@@ -16,3 +18,16 @@ fn main() {
     }
     println!("{}", count)
 }
+
+// fn main() {
+//     let mut count: u32 = 0;
+//     for _ in 0..10000 {
+//         let layout: Array2<u8> = map_generator();
+//         for row in layout.rows() {
+//             for &val in row {
+//                 if val != 0 {count += 1}
+//             }
+//         }
+//     }
+//     println!("{}", count/10000)
+// }
