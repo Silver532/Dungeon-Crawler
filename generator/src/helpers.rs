acdfa6491134cf8906f849a11ebda0ebaf287a93
Silver532 <silver532mc@gmail.com>
2026-04-14 10:17:31 -0400

    Tweak and add constants

--- generator/src/helpers.rs
+++ generator/src/helpers.rs
@@ -19,8 +19,8 @@ pub fn init_rng(input: Option<&str>) -> StdRng {
 pub mod s1 {
     pub const DUNGEON_SIZE: usize = 18;
     pub const MID: usize = DUNGEON_SIZE / 2;
-    pub const MAX_BOX_DIM: usize = 11;
-    pub const BOX_COUNT: usize = 4;
+    pub const MAX_BOX_DIM: usize = 13;
+    pub const BOX_COUNT: usize = 5;
     pub const ERODE_COUNT: usize = 5;
 
     pub const NULL:  u8 = 0b00000000;
@@ -29,4 +29,25 @@ pub mod s1 {
     pub const EAST:  u8 = 0b00000010;
     pub const SOUTH: u8 = 0b00000100;
     pub const WEST:  u8 = 0b00001000;
+    pub const DIR_BITS: [u8; 4] = [1,2,4,8];
+    pub const OPP_BITS: [u8; 4] = [4,8,1,2];
+    pub const DY_DX: [[i8; 2]; 4] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    pub const MASK_TO_INDICES: [&'static [usize]; 16] = [
+        &[],          // 0000
+        &[0],         // 0001 N
+        &[1],         // 0010 E
+        &[0, 1],      // 0011 NE
+        &[2],         // 0100 S
+        &[0, 2],      // 0101 NS
+        &[1, 2],      // 0110 ES
+        &[0, 1, 2],   // 0111 NES
+        &[3],         // 1000 W
+        &[0, 3],      // 1001 NW
+        &[1, 3],      // 1010 EW
+        &[0, 1, 3],   // 1011 NEW
+        &[2, 3],      // 1100 SW
+        &[0, 2, 3],   // 1101 NSW
+        &[1, 2, 3],   // 1110 ESW
+        &[0, 1, 2, 3],// 1111 NESW
+    ];
 }
