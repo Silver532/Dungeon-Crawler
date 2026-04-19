eaa866c6690b5f872751434ffe39c27a679425cf
Silver532 <silver532mc@gmail.com>
2026-04-18 23:42:39 -0400

    update output on lib.rs

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -5,9 +5,10 @@ mod stage_2;
 //mod stage_3;
 pub mod helpers;
 
-pub fn map_generator() -> Array2<u8> {
+pub fn map_generator() -> (Array2<u8>, Array2<u8>) {
     let mut rng = helpers::init_rng(None);
     
-    let layout = stage_1::generate_layout(&mut rng);
-    return layout
+    let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
+    let (shape_map, theme_map) = stage_2::plan_rooms(&layout, &mut rng);
+    (shape_map, theme_map)
 }
