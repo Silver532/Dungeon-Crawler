8415578ca997ce7d6d59fcd7742c0481390b5558
Silver532 <silver532mc@gmail.com>
2026-04-18 23:46:48 -0400

    Add intermediate functions in lib.rs

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -5,6 +5,17 @@ mod stage_2;
 //mod stage_3;
 pub mod helpers;
 
+pub fn run_stage_1() -> Array2<u8> {
+    let mut rng = helpers::init_rng(None);
+    stage_1::generate_layout(&mut rng)
+}
+
+pub fn run_stage_2() -> (Array2<u8>, Array2<u8>) {
+    let mut rng = helpers::init_rng(None);
+    let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
+    stage_2::plan_rooms(&layout, &mut rng)
+}
+
 pub fn map_generator() -> (Array2<u8>, Array2<u8>) {
     let mut rng = helpers::init_rng(None);
     
