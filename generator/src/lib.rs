bb0f2c6a5e740bec7f3c3645b338fa6788a51582
Silver532 <silver532mc@gmail.com>
2026-04-21 10:16:01 -0400

    fix lib.rs to use seeds

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -5,19 +5,19 @@ mod stage_2;
 //mod stage_3;
 pub mod helpers;
 
-pub fn run_stage_1() -> Array2<u8> {
-    let mut rng = helpers::init_rng(None);
+pub fn run_stage_1(seed: u64) -> Array2<u8> {
+    let mut rng = helpers::init_rng(seed);
     stage_1::generate_layout(&mut rng)
 }
 
-pub fn run_stage_2() -> (Array2<u8>, Array2<u8>) {
-    let mut rng = helpers::init_rng(None);
+pub fn run_stage_2(seed: u64) -> (Array2<u8>, Array2<u8>) {
+    let mut rng = helpers::init_rng(seed);
     let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
     stage_2::plan_rooms(&layout, &mut rng)
 }
 
-pub fn map_generator() -> (Array2<u8>, Array2<u8>) {
-    let mut rng = helpers::init_rng(None);
+pub fn map_generator(seed: u64) -> (Array2<u8>, Array2<u8>) {
+    let mut rng = helpers::init_rng(seed);
     
     let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
     let (shape_map, theme_map) = stage_2::plan_rooms(&layout, &mut rng);
