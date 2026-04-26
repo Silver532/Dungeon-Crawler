3288e7b228439f680b6b965eece22573dc92337b
kufbsrieewz <kufbsrieewz@gmail.com>
2026-04-26 17:35:40 -0400

    Initialize item system

---
+++ items/src/lib.rs
@@ -0,0 +1,33 @@
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Position {
+    TOP = 0,
+    MIDDLE,
+    BOTTOM,
+    SPECIAL,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Part {
+    name: String,
+    position: Position,
+    compatibility: u32
+}
+impl Part {
+    fn new(name: &str, position: Position, compatibility: u32) -> Part {
+        Part {name: name.to_string(), position: position, compatibility: compatibility}
+    }
+    fn compatible(&self, other: &Part) -> bool {
+        self.compatibility & other.compatibility != 0u32
+    }
+}
+struct Item {
+    top: Part,
+    middle: Part,
+    bottom: Part
+}
+impl Item {
+    fn new(top: &Part, middle: &Part, bottom: &Part) -> Item {
+        Item {top: top.clone(), middle: middle.clone(), bottom: bottom.clone()}
+    }
+}
