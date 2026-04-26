8b0584df3806fba15c8542fd4587d3a4a2c16ea7
Silver532 <silver532mc@gmail.com>
2026-04-25 21:49:23 -0400

    modify individual stage 2 run function now it returns layout as well for ease of visualization

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -10,10 +10,10 @@ pub fn run_stage_1(seed: u64) -> Array2<u8> {
     stage_1::generate_layout(&mut rng)
 }
 
-pub fn run_stage_2(seed: u64) -> (Array2<u8>, Array2<u8>) {
+pub fn run_stage_2(seed: u64) -> ((Array2<u8>, Array2<u8>), Array2<u8>) {
     let mut rng = helpers::init_rng(seed);
     let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
-    stage_2::plan_rooms(&layout, &mut rng)
+    (stage_2::plan_rooms(&layout, &mut rng), layout)
 }
 
 pub fn map_generator(seed: u64) -> (Array2<u8>, Array2<u8>) {
