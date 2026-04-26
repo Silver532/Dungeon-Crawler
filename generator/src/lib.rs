d826a1532cd3ca0fc8cd94cef5d8d5c03d8410ca
Silver532 <silver532mc@gmail.com>
2026-04-26 12:43:54 -0400

    Add stage 3 placeholder

--- generator/src/lib.rs
+++ generator/src/lib.rs
@@ -2,7 +2,7 @@ use ndarray::Array2;
 
 mod stage_1;
 mod stage_2;
-//mod stage_3;
+mod stage_3;
 pub mod helpers;
 
 pub fn run_stage_1(seed: u64) -> Array2<u8> {
@@ -16,10 +16,18 @@ pub fn run_stage_2(seed: u64) -> ((Array2<u8>, Array2<u8>), Array2<u8>) {
     (stage_2::plan_rooms(&layout, &mut rng), layout)
 }
 
+pub fn run_stage_3(seed: u64) -> Array2<u8> {
+    let mut rng = helpers::init_rng(seed);
+    let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
+    let (shapes, themes) = stage_2::plan_rooms(&layout, &mut rng);
+    stage_3::build_tilemap(layout, shapes, &themes, rng)
+}
+
 pub fn map_generator(seed: u64) -> (Array2<u8>, Array2<u8>) {
     let mut rng = helpers::init_rng(seed);
     
     let layout: Array2<u8> = stage_1::generate_layout(&mut rng);
-    let (shape_map, theme_map) = stage_2::plan_rooms(&layout, &mut rng);
-    (shape_map, theme_map)
+    let (shapes, themes) = stage_2::plan_rooms(&layout, &mut rng);
+    let tilemap: Array2<u8> = stage_3::build_tilemap(layout, shapes, &themes, rng);
+    (tilemap, themes)
 }
---
+++ generator/src/stage_3.rs
@@ -0,0 +1,6 @@
+use ndarray::Array2;
+use rand::rngs::StdRng;
+
+pub fn build_tilemap(layout: Array2<u8>, shapes: Array2<u8>, themes: &Array2<u8>, rng: StdRng) -> Array2<u8> {
+    layout
+}
