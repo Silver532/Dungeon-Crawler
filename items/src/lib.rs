4252c789cd9669ba8b323cefd9bc2a7146dea49d
Silver532 <silver532mc@gmail.com>
2026-05-14 20:33:01 -0400

    Comment out item code so it stops bothering me with warnings

--- items/src/lib.rs
+++ items/src/lib.rs
@@ -1,34 +1,34 @@
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Position {
-    TOP = 0,
-    MIDDLE,
-    BOTTOM,
-    SPECIAL,
-}
+// #[repr(u8)]
+// #[derive(Clone, Copy, Debug, PartialEq)]
+// enum Position {
+//     TOP = 0,
+//     MIDDLE,
+//     BOTTOM,
+//     SPECIAL,
+// }
 
-#[derive(Clone, Debug, PartialEq)]
-struct Part {
-    name: String,
-    position: Position,
-    compatibility: u32
-}
-impl Part {
-    fn new(name: &str, position: Position, compatibility: u32) -> Part {
-        Part {name: name.to_string(), position: position, compatibility: compatibility}
-    }
-    fn compatible(&self, other: &Part) -> bool {
-        self.compatibility & other.compatibility != 0u32
-    }
-}
-struct Item {
-    top: Part,
-    middle: Part,
-    bottom: Part,
-    special: Option<Part>
-}
-impl Item {
-    fn new(top: Part, middle: Part, bottom: Part, special: Option<Part>) -> Item {
-        Item {top: top, middle: middle, bottom: bottom, special: special}
-    }
-}
+// #[derive(Clone, Debug, PartialEq)]
+// struct Part {
+//     name: String,
+//     position: Position,
+//     compatibility: u32
+// }
+// impl Part {
+//     fn new(name: &str, position: Position, compatibility: u32) -> Part {
+//         Part {name: name.to_string(), position: position, compatibility: compatibility}
+//     }
+//     fn compatible(&self, other: &Part) -> bool {
+//         self.compatibility & other.compatibility != 0u32
+//     }
+// }
+// struct Item {
+//     top: Part,
+//     middle: Part,
+//     bottom: Part,
+//     special: Option<Part>
+// }
+// impl Item {
+//     fn new(top: Part, middle: Part, bottom: Part, special: Option<Part>) -> Item {
+//         Item {top: top, middle: middle, bottom: bottom, special: special}
+//     }
+// }
